1a60f1a8414fbbcbf48d139146309675146d249b
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2023-12-01 16:06:57 +0100

    solve challenge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from "react";
 import "./styles.css";
 
+function Sum({ valueA, valueB }) {
+  return (
+    <div>
+      <h1>Sum</h1>
+      <p>
+        the sum of <strong>{valueA}</strong> and <strong> {valueB}</strong> is{" "}
+        <strong className="sum">{valueA + valueB}</strong>
+      </p>
+    </div>
+  );
+}
+
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Sum valueA={3} valueB={1} />;
 }
